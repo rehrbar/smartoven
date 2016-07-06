218f3ab378e73df9c3226db22790b1f54deae9e4
Roman Ehrbar <r1ehrbar@hsr.ch>
2016-07-07 00:42:04 +0200

    Added publishing events on mqtt topic.

--- buttonListener.py
+++ buttonListener.py
@@ -2,23 +2,34 @@
 
 import RPi.GPIO as GPIO
 import time
+import paho.mqtt.client as mqtt
 print("RPi.GPIO version: " + GPIO.VERSION)
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(2,GPIO.OUT)
-
 buttonPins = {12:'UP',13:'RIGHT',14:'DOWN',15:'LEFT'}
+client = mqtt.Client()
 
+# callbacks
 def button_callback(channel):
     print("Callback on " + buttonPins[channel])
+    client.publish("/button", buttonPins[channel])
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected to broker with result code " + str(rc))
+
+GPIO.setmode(GPIO.BCM) # BCM numbering should be the one as labelled on the shield
+GPIO.setup(2,GPIO.OUT) # LED output
+
+# Enable all buttons and add event
 for pin in buttonPins:
     GPIO.setup(pin,GPIO.IN)
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_callback, bouncetime=400)
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_callback, bouncetime=600)
 
+client.on_connect = on_connect
+client.connect("localhost", 1883)
+client.loop_start()
 
 try:
+    # Blink led as status light
     while True:
         GPIO.output(2,True)
         time.sleep(2.0)
@@ -27,5 +38,5 @@ try:
 
 except KeyboardInterrupt:
     print("^C received, shutting down client")
-    #client.loop_stop()
+    client.loop_stop()
     GPIO.cleanup()
